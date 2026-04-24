7b0b6df54bfa9ef4b64285eb7a24de41d32e0237
dorisrosario <2240028@ipisa.edu.do>
2026-04-24 08:27:25 -0400

    mi primer commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { engine } = require('express-handlebars');
+
+const app = express();
+
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+
+app.listen(3000, () => {
+    console.log('Servidor corriendo en http://localhost:3000');
+});
